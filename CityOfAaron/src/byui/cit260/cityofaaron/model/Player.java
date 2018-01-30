36613870aa5291b1387b14ce24410a4e741b41b2
JChantry <JChantry@VAAC-Chantry.VAAC.local>
2018-01-30 14:59:04 -0700

    1/30/18 - went through the instructions and added the code

--- CityOfAaron/src/byui/cit260/cityofaaron/model/Player.java
+++ CityOfAaron/src/byui/cit260/cityofaaron/model/Player.java
@@ -5,10 +5,62 @@
  */
 package byui.cit260.cityofaaron.model;
 
+import java.io.Serializable;
+import java.util.Objects;
+
 /**
  *
  * @author JChantry
  */
-public class Player {
+public class Player implements Serializable{
+    
+    // Class instance variables
+    private String name;
+
+    public Player() {
+    }
+    
+    
+    //get method for Name
+    public String getName() {
+        return name;
+    }
+    
+    //set method for name
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 89 * hash + Objects.hashCode(this.name);
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" + "name=" + name + '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Player other = (Player) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
     
 }
