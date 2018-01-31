81b8a2817c01d97523118e106ea1c6b8a2ef8227
JChantry <JChantry@VAAC-Chantry.VAAC.local>
2018-01-31 10:01:44 -0700

    Updated with Bro. DeBry's suggestions

--- CityOfAaron/src/byui/cit260/cityofaaron/model/TeamMember.java
+++ CityOfAaron/src/byui/cit260/cityofaaron/model/TeamMember.java
@@ -11,46 +11,22 @@ import java.io.Serializable;
  */
 public enum TeamMember implements Serializable {
     
-    Jason("Jason", "He is one of the team members of the team.", new Point(1,1)),
-    Ken("Ken", "He is one of the team members of the team.", new Point(1,2)),
-    Joshua("Joshua", "He is one of the team members of the team.", new Point(1,3));    
+    Jason("Jason", "He is one of the team members of the team.");
+    Ken("Ken", "He is one of the team members of the team.");
+    Joshua("Joshua", "He is one of the team members of the team.");
+    
     private String name;
     private String title;
-    private Point coordinates;
 
-    TeamMember(String name, String title, Point coordinates) {
+    TeamMember(String name, String title) {
         this.name = name;
         this.title = title;
-        this.coordinates = coordinates;
+ 
     }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public Point getCoordinates() {
-        return coordinates;
-    }
-
-    public void setCoordinates(Point coordinates) {
-        this.coordinates = coordinates;
-    }
-
+   
     @Override
     public String toString() {
-        return "TeamMember{" + "name=" + name + ", title=" + title + ", coordinates=" + coordinates + '}';
+        return "TeamMember{" + "name=" + name + ", title=" + title + '}';
     }
     
 
