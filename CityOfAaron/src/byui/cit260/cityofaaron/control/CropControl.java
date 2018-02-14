89d9d0f62efcba55ac6b7334f2915af9d074e6d0
JChantry <JChantry@VAAC-Chantry.VAAC.local>
2018-02-14 10:37:23 -0700

    adjusted sellLand method

---
+++ CityOfAaron/src/byui/cit260/cityofaaron/control/CropControl.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.cityofaaron.control;
+
+import byui.cit260.cityofaaron.model.CropData;
+import java.io.Serializable;
+/**
+ *
+ * @author JChantry
+ */
+public class CropControl implements Serializable{
+  
+    
+    
+    // The sellLand method
+    // Purpose: To sell land
+    // Parameters: the price of land and the number of acres to sell
+    // Returns: the number of acres left after the sale
+    // Pre-conditions: acres to sell must be positive
+    // and <= acresOwned
+    public static int sellLand(int landPrice, int acresToSell, CropData cropData) {
+        
+    }
+
+    
+}
