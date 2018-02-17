5e22bf56e54c17b02a86dfefd004a86265fc362c
JChantry <JChantry@VAAC-Chantry.VAAC.local>
2018-02-16 19:49:40 -0700

    Created feedPeople method in CropControl class and tested 8 test cases

--- CityOfAaron/test/byui/cit260/cityofaaron/control/CropControlTest.java
+++ CityOfAaron/test/byui/cit260/cityofaaron/control/CropControlTest.java
@@ -86,6 +86,71 @@ public class CropControlTest {
 
     }
 
+
+
+    /**
+     * Test of feedPeople method, of class CropControl.
+     */
+    @Test
+    public void testFeedPeople() {
+        System.out.println("feedPeople Test Case 1");
+        CropData theCrops = new CropData();
+        theCrops.setWheatInStore(2000);
+        int amountToFeedPeople = 500;
+        int expResult = 1500;
+        int result = CropControl.feedPeople(amountToFeedPeople, theCrops);
+        assertEquals(expResult, result);
+        
+        System.out.println("feedPeople Test Case 2");
+        theCrops.setWheatInStore(1500);
+        amountToFeedPeople = 750;
+        expResult = 750;
+        result = CropControl.feedPeople(amountToFeedPeople, theCrops);
+        assertEquals(expResult, result);
+        
+        System.out.println("feedPeople Test Case 3");
+        theCrops.setWheatInStore(2000);
+        amountToFeedPeople = -5;
+        expResult = -1;
+        result = CropControl.feedPeople(amountToFeedPeople, theCrops);
+        assertEquals(expResult, result);
+
+        System.out.println("feedPeople Test Case 4");
+        theCrops.setWheatInStore(2000);
+        amountToFeedPeople = 2500;
+        expResult = -2;
+        result = CropControl.feedPeople(amountToFeedPeople, theCrops);
+        assertEquals(expResult, result);     
+
+        System.out.println("feedPeople Test Case 5");
+        theCrops.setWheatInStore(3000);
+        amountToFeedPeople = -200;
+        expResult = -1;
+        result = CropControl.feedPeople(amountToFeedPeople, theCrops);
+        assertEquals(expResult, result);
+        
+        System.out.println("feedPeople Test Case 6");
+        theCrops.setWheatInStore(2500);
+        amountToFeedPeople = 0;
+        expResult = 2500;
+        result = CropControl.feedPeople(amountToFeedPeople, theCrops);
+        assertEquals(expResult, result);
+        
+        System.out.println("feedPeople Test Case 7");
+        theCrops.setWheatInStore(2500);
+        amountToFeedPeople = 2500;
+        expResult = 0;
+        result = CropControl.feedPeople(amountToFeedPeople, theCrops);
+        assertEquals(expResult, result);
+        
+        System.out.println("feedPeople Test Case 8");
+        theCrops.setWheatInStore(500);
+        amountToFeedPeople = 500;
+        expResult = 0;
+        result = CropControl.feedPeople(amountToFeedPeople, theCrops);
+        assertEquals(expResult, result);
+    }
+
     /**
      * Test of harvestCrops method, of class CropControl.
      
